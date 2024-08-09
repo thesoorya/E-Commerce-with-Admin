7a45fa8d8927cfeeaa6c1e965c25f6e46d7c50a3
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-09 15:39:56 +0530

    url updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
-import './App.css'
-import DisplayButtons from './components/Displaybuttons/DisplayButtons'
-import AllProduct from './components/AllProduct/AllProduct'
-import AddProduct from './components/AddProduct/AddProduct'
+import React from "react";
+import "./App.css";
+import List from "./pages/List/List";
+import Add from "./pages/Add/Add";
+import Order from "./pages/Order/Order";
+import Buttons from "./components/Buttons/Buttons";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-
-  const [display, setDisplay] = useState('All')
+  const url = "http://localhost:5000";
 
   return (
-    <div>
-      <DisplayButtons setDisplay={setDisplay} />
-      
-      {display === 'All' && <AllProduct />}
-      {display === 'Add' && <AddProduct />}
+    <div className="app">
+      <ToastContainer />
+      <Buttons />
+      <Routes>
+        <Route path="/" element={<Navigate to="/list" />} />
+        <Route path="/list" element={<List url={url} />} />
+        <Route path="/add" element={<Add url={url} />} />
+        <Route path="/order" element={<Order url={url} />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
