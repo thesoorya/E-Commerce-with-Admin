1c46beb59d639d52e53c50a856eb2ef5e9157231
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-13 16:54:13 +0530

    user route and model created

---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+    minimize: false,
+  }
+);
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+module.exports = User;
